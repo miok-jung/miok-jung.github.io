71aef442ba4e9b48a7a60e1385d0f9fbd8042fd4
miok-jung <miok.dev@gmail.com>
2024-09-01 13:17:52 +0900

    perf: 라우트 설정

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,7 +20,9 @@ import routes from './routes';
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -30,6 +32,11 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
+
+  // NOTE: 라우터 설정
+  Router.beforeEach((to) => {
+    document.title = `${to.meta.title}`;
   });
 
   return Router;
