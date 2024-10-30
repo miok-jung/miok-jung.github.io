f26bb8b69bea286fc6846c7a7e2cdfdf772a3e9d
miok-jung <miok-jung@gmail.com>
2024-10-30 17:39:23 +0900

    update: img, video 만들고 실행시켜보기

--- src/components/study/GallayJS.vue
+++ src/components/study/GallayJS.vue
@@ -1,11 +1,16 @@
 <template>
   <div ref="canvasRef"></div>
+  <div ref="imageRef"></div>
+  <div ref="videoRef"></div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
 const canvasRef = ref<HTMLElement | undefined>(undefined);
+const imageRef = ref<HTMLElement | undefined>(undefined);
+const videoRef = ref<HTMLElement | undefined>(undefined);
+
 // NOTE: life-cycle
 onMounted(() => {
   const canvas = document.createElement('canvas');
@@ -17,56 +22,41 @@ onMounted(() => {
   if (canvasRef.value) canvasRef.value.appendChild(canvas);
   const ctx = canvas.getContext('2d');
   if (ctx) {
-    // ctx.strokeStyle = '#000';
-    // ctx.lineWidth = 5;
-    // ctx.fillStyle = '#aaa';
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    // // 사각형
-    // ctx.fillStyle = '#f00';
-    // ctx.fillRect(100, 100, 200, 200);
-    // ctx.strokeRect(100, 100, 200, 200);
-    // // 원형
-    // ctx.fillStyle = '#0f0';
-    // ctx.beginPath();
-    // ctx.arc(200, 200, 50, 0, 2 * Math.PI);
-    // ctx.fill();
-    // ctx.stroke();
-    // // 선
-    // ctx.beginPath();
-    // ctx.strokeStyle = '#00f';
-    // ctx.moveTo(400, 100);
-    // ctx.lineTo(400, 300);
-    // ctx.stroke();
-    // // 다각형
-    // ctx.beginPath();
-    // ctx.moveTo(450, 100);
-    // ctx.lineTo(450, 300);
-    // ctx.lineTo(650, 300);
-    // ctx.lineTo(450, 100);
-    // ctx.fill();
-    // ctx.stroke();
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 5;
+    ctx.fillStyle = '#aaa';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = '#fff';
-    // ctx.fillRect(50, 100, 550, 200);
+    // image
+    const image = document.createElement('img');
+    canvasRef.value?.appendChild(image);
+    image.src = 'https://picsum.photos/1500/1500';
+    // setTimeout(() => {
+    //   ctx.drawImage(image, 0, 0);
+    // }, 1000);
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0);
+    };
 
-    ctx.font = 'bold 30px Arial';
-    // ctx.fillStyle = '#000';
-    // ctx.textBaseline = 'top';
-    // ctx.fillText('hello', 50, 100);
-    ctx.lineWidth = 1;
-    ctx.beginPath();
-    ['top', 'hanging', 'middle', 'alphabetic', 'ideographic', 'bottom'].forEach((baseline, i) => {
-      const y = 50 + i * 50;
-      ctx.strokeStyle = '#f00';
-      ctx.moveTo(50, y);
-      ctx.lineTo(550, y);
-      ctx.stroke();
-      ctx.textBaseline = baseline as CanvasTextBaseline; // 명시적 타입 단언
-      ctx.strokeStyle = '#000';
-      ctx.fillText(baseline, 50, y);
-      ctx.strokeText(baseline, 50, y);
-    });
+    // video
+    const video = document.createElement('video');
+    canvasRef.value?.appendChild(video);
+    video.src = '/src/assets/study/videos/01.mp4';
+    video.onloadeddata = () => {
+      video.autoplay = true;
+      video.playsInline = true;
+      video.loop = true;
+      video.muted = true;
+      console.log('load', video.autoplay);
+      draw();
+    };
+    function draw() {
+      setTimeout(() => {
+        draw();
+        ctx?.drawImage(video, 100, 100, 200, 200);
+      }, 1000 / 10);
+    }
   }
 });
 </script>
