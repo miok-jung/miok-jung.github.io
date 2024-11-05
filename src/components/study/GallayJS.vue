d52dffa60ed6d641c92a8441f52cb1adc2b3bafa
miok-jung <miok-jung@gmail.com>
2024-11-05 15:33:53 +0900

    update: Canvas API 재활용 및 재업로드

--- src/components/study/GallayJS.vue
+++ src/components/study/GallayJS.vue
@@ -1,13 +1,15 @@
 <template>
-  <div ref="canvasRef"></div>
-  <div ref="imageRef"></div>
-  <div ref="videoRef"></div>
+  <div class="q-mb-md" ref="canvasRef" />
+  <div class="q-mb-md" ref="textCanvasRef" />
+  <div class="q-mb-md" ref="imageRef" />
+  <div ref="videoRef" />
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
 const canvasRef = ref<HTMLElement | undefined>(undefined);
+const textCanvasRef = ref<HTMLElement | undefined>(undefined);
 const imageRef = ref<HTMLElement | undefined>(undefined);
 const videoRef = ref<HTMLElement | undefined>(undefined);
 
@@ -21,70 +23,62 @@ type typeBall = {
 
 // NOTE: life-cycle
 onMounted(() => {
-  const canvas = document.createElement('canvas');
+  // SECTION: 첫번째 도형 연습
+  const canvas1 = document.createElement('canvas');
   const width = 720;
-  const height = 250;
-  canvas.width = width * window.devicePixelRatio;
-  canvas.height = height * window.devicePixelRatio;
-  canvas.style.width = width + 'px';
-  canvas.style.height = height + 'px';
-  if (canvasRef.value) canvasRef.value.appendChild(canvas);
-  const ctx = canvas.getContext('2d');
-
+  const height = 720;
+  canvas1.width = width * window.devicePixelRatio;
+  canvas1.height = height * window.devicePixelRatio;
+  canvas1.style.width = width + 'px';
+  canvas1.style.height = height + 'px';
+  if (canvasRef.value) canvasRef.value.appendChild(canvas1);
+  const ctx = canvas1.getContext('2d');
   if (ctx) {
     ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
     ctx.fillRect(0, 0, width, height);
 
-    const audio = new Audio(); // document.createElement('audio'); 와 동일한 방식
-    audio.src = '/src/assets/study/audio/01.mp3';
-    // audio.onloadeddata = () => {
-    //   audio.play();
-    // };
+    // NOTE: 사각형
+    ctx.fillStyle = '#F68657';
+    ctx.fillRect(100, 100, 200, 200);
+    // 사각형 border
+    ctx.strokeStyle = '#A593E0';
+    ctx.lineWidth = 5;
+    ctx.strokeRect(90, 90, 220, 220);
 
-    canvas.onclick = () => {
-      audio.play();
-      const analyser = connectAudio();
-      draw(analyser);
-    };
+    // NOTE: 원형
+    ctx.beginPath();
+    ctx.arc(500, 200, 100, 0, 2 * Math.PI);
+    ctx.fillStyle = '#80d4f6';
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = '#534847';
+    ctx.fill();
+    ctx.stroke();
 
-    function connectAudio() {
-      // window.webkitAudioContext의 경우 구 버전 Safari까지 지원을 하는게 아니라면 굳이 필요가 없음
-      const audioContext = new window.AudioContext();
-      const audioSource = audioContext.createMediaElementSource(audio);
-      const analyser = audioContext.createAnalyser();
-      analyser.fftSize = 256;
-      audioSource.connect(analyser);
-      analyser.connect(audioContext.destination);
-      return analyser;
-    }
+    // NOTE: 선
+    ctx.beginPath();
+    ctx.strokeStyle = '#fd999a';
+    ctx.moveTo(100, 400);
+    ctx.lineTo(100, 600);
+    ctx.stroke();
 
-    function draw(analyser: AnalyserNode) {
-      const bufferLength = analyser.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-      analyser.getByteFrequencyData(dataArray);
-
-      if (ctx === null) throw new Error('ctx New Error Audio');
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
-      ctx.fillRect(0, 0, width, height);
-
-      const barWidth = (width / bufferLength) * 2;
-      let barHeight = 0;
-      let x = 0;
-
-      ctx?.save(); // ctx.restore(); -> 기존에 있던 설정 값들을 stact에 담는다?
-      for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-
-        if (ctx === null) throw new Error('ctx New Error Audio');
-        ctx.fillStyle = '#55b';
-        ctx.fillRect(x, height - barHeight, barWidth, barHeight);
-        x += barWidth;
-      }
-
-      ctx?.restore();
-      requestAnimationFrame(() => draw(analyser));
-    }
+    // NOTE: 다각형
+    ctx.beginPath();
+    ctx.moveTo(400, 400);
+    ctx.lineTo(400, 600);
+    ctx.lineTo(650, 600);
+    ctx.lineTo(400, 400);
+    ctx.lineCap = 'round';
+    ctx.setLineDash([4, 20]);
+    ctx.fillStyle = '#c72e45';
+    ctx.fill();
+    ctx.strokeStyle = '#cff0da';
+    ctx.stroke();
   }
+  // !SECTION
+
+  // SECTION 두번째 텍스트 연습
+  // const canvas2 =
+  // !SECTION
 });
 </script>
 
