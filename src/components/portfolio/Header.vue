a02fb74ecaebf6152ad8826570e5c1c58a155d05
miok-jung <miok.dev@gmail.com>
2025-01-28 13:22:14 +0900

    feat: Main Portfolio Header 분리

---
+++ src/components/portfolio/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn flat dense round icon="menu" aria-label="Menu" @click="$emit('toggle-left-drawer')" />
+      <q-toolbar-title> Miok Jung's Portfolio </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+  (e: 'toggle-left-drawer'): void // ✅ 부모(MainLayout.vue)에서 상태를 변경할 수 있도록 이벤트 정의
+}>()
+</script>
+
+<style scoped></style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,12 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-        <q-toolbar-title> Miok Jung's Portfolio </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
+    <Header :left-drawer-open="leftDrawerOpen" @toggle-left-drawer="toggleLeftDrawer" />
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header>Portfolio List</q-item-label>
-
         <MainSidebar v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -24,6 +16,12 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import MainSidebar, { type MainSidebarProps } from 'components/MainSidebar.vue'
+import Header from 'src/components/portfolio/Header.vue'
+
+const leftDrawerOpen = ref(false)
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
 
 const linksList: MainSidebarProps[] = [
   {
@@ -33,10 +31,4 @@ const linksList: MainSidebarProps[] = [
     link: 'https://quasar.dev',
   },
 ]
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
 </script>
