946a75e275e7af29fca514b70c4ce64576cb2e2a
miok-jung <miok-jung@users.noreply.github.com>
2025-02-06 14:19:06 +0000

    deploy: 83c0a94c1ff3932e90b285f2642d85126f09aa6e

---
+++ assets/use-timeout-CMgoOq6Y.js
@@ -0,0 +1,1 @@
+import{a as u}from"./vm-Dp50bsxd.js";import{R as i,l as m,g as s}from"./index-CprQqKhJ.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
