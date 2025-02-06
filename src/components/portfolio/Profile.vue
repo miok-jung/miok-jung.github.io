83b46769ec7e43f4d85cf36b0d9e56cce047a038
miok-jung <miok.dev@gmail.com>
2025-02-06 23:16:50 +0900

    feat: 포트폴리오 기본 틀 업데이트

---
+++ src/components/portfolio/Profile.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="profile-wrap">
+    <article>
+      <q-img class="avatar-image" :src="avatar" />
+    </article>
+    <article class="avatar-information">
+      <p :class="$q.screen.width < 769 ? 'text-h6' : 'text-h5'">이름: 정미옥</p>
+    </article>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+import avatar from '/images/avatar.jpg';
+
+const $q = useQuasar();
+</script>
+
+<style lang="scss" scoped>
+@import '/src/css/mixins.scss';
+
+.profile-wrap {
+  display: flex;
+  flex-flow: row;
+  article {
+    &:first-child {
+      margin-right: 48px;
+      @include media('sm') {
+        margin-right: 24px;
+      }
+      @include media('xs') {
+        margin-right: 16px;
+      }
+    }
+  }
+  .avatar-image {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    @include media('sm') {
+      width: 100px;
+      height: 100px;
+    }
+    @include media('xs') {
+      width: 70px;
+      height: 70px;
+    }
+  }
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,23 +1,43 @@
 <template>
   <q-page class="row items-center justify-evenly" :style-fn="mainStyle">
-    <ScrollArea :style="{ height: `calc(${$q.screen.height}px - ${objects}px)` }">
-      <div style="border: 1px solid red; height: 200vh">TEST</div>
+    <ScrollArea
+      :style="{
+        width: ui.isSidebarShow
+          ? `calc(${$q.screen.width}px - 300px)`
+          : `calc(${$q.screen.width}px)`,
+        height: objects
+          ? `calc(100vh - ${objects}px - (${$q.screen.height}px * 0.1))`
+          : 'calc(100vh - (${$q.screen.height}px * 0.1))',
+      }"
+    >
+      <Profile />
     </ScrollArea>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useQuasar } from 'quasar';
 import ScrollArea from 'src/components/common/ScrollArea.vue';
-import { ref } from 'vue';
+import { useUiStore } from 'src/stores/ui';
+import Profile from 'src/components/portfolio/Profile.vue';
 
 const $q = useQuasar();
+const ui = useUiStore();
 
 const objects = ref<number>(0);
 function mainStyle(object: number) {
   objects.value = object;
   return {
-    minHeight: object ? `calc(100vh - ${object}px)` : '100vh',
+    padding: `calc(${$q.screen.height}px * 0.05) calc(${$q.screen.width}px * 0.05) `,
+    width: ui.isSidebarShow
+      ? `calc(${$q.screen.width}px - 300px - (${$q.screen.height}px * 0.1))`
+      : `calc(${$q.screen.width}px - (${$q.screen.height}px * 0.1))`,
+    minHeight: object
+      ? `calc(100vh - ${object}px (${$q.screen.height}px * 0.1))`
+      : 'calc(100vh - (${$q.screen.height}px * 0.1))',
+    boxSizing: 'border-box',
   };
 }
 </script>
+<style scoped lang="scss"></style>
