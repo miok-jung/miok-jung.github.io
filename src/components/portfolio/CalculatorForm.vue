723ca4ee7c186959fac2ad31d716fa7c20c6d4a9
miok-jung <miok.dev@gmail.com>
2025-02-07 21:21:22 +0900

    feat: 계산기 기본 틀 제작

--- src/components/portfolio/CalculatorForm.vue
+++ src/components/portfolio/CalculatorForm.vue
@@ -1,14 +1,55 @@
 <template>
+  <!-- TODO: 1. 버튼을 클릭 혹은 숫자를 클릭하면 해당 숫자 혹은 연산자가 입력이 된다. -->
+  <!-- TODO: 2. 엔터 혹은 =버튼을 클릭시 계산이 된다. -->
+  <!-- TODO: 큰 숫자를 입력시 콤마가 자동으로 적용이 되도록 수정을 한다 -->
+  <!-- TODO: 숫자를 잘 못입력을 할 경우 지우기 버튼 혹은 backspace 키보드 버튼으로 클릭시 한개씩 지울 수 있다 -->
+  <!-- TODO: 소수점 계산은 4자리로 제한한다 -->
+  <!-- TODO: *, /먼저 연산자 계산을 한다 -->
+  <!-- TODO: 이후 +, -는 나중에 계산을 한다 -->
+  <!-- TODO: 괄호가 있다면 최우선적으로 계산을 한다 -->
+  <!-- TODO: 괄호 키보드 입력을 인식한다 -->
   <div class="calculator-wrap">
     <q-card class="row no-shadow" bordered>
       <q-card-section class="col left-wrap">
-        <div>
-          <q-input v-model="input" />
+        <div class="q-gutter-y-sm">
+          <q-input v-model="input" dense outlined readonly />
+          <q-input v-model="result" dense outlined readonly />
         </div>
-        <div>카드 입력 버튼 공간</div>
+        <div class="q-gutter-y-sm">
+          <div class="row no-wrap q-gutter-x-sm">
+            <q-btn class="col" style="" outline />
+            <q-btn class="col" style="" outline />
+            <q-btn class="col" style="" outline />
+            <q-btn class="col" label="X" outline @click="onClickValue(' * ')" />
+          </div>
+          <div class="row no-wrap q-gutter-x-sm">
+            <q-btn class="col" label="7" outline @click="onClickValue('7')" />
+            <q-btn class="col" label="8" outline @click="onClickValue('8')" />
+            <q-btn class="col" label="9" outline @click="onClickValue('9')" />
+            <q-btn class="col" label="X" outline @click="onClickValue(' * ')" />
+          </div>
+          <div class="row no-wrap q-gutter-x-sm">
+            <q-btn class="col" label="4" outline @click="onClickValue('4')" />
+            <q-btn class="col" label="5" outline @click="onClickValue('5')" />
+            <q-btn class="col" label="6" outline @click="onClickValue('6')" />
+            <q-btn class="col" label="-" outline @click="onClickValue(' - ')" />
+          </div>
+          <div class="row no-wrap q-gutter-x-sm">
+            <q-btn class="col" label="1" outline @click="onClickValue('1')" />
+            <q-btn class="col" label="2" outline @click="onClickValue('2')" />
+            <q-btn class="col" label="3" outline @click="onClickValue('3')" />
+            <q-btn class="col" label="+" outline @click="onClickValue(' + ')" />
+          </div>
+          <div class="row no-wrap q-gutter-x-sm">
+            <q-btn class="col" label="+/-" outline @click="onClickValue('+/-')" />
+            <q-btn class="col" label="0" outline @click="onClickValue('0')" />
+            <q-btn class="col" label="." outline @click="onClickValue('.')" />
+            <q-btn class="col" label="=" outline />
+          </div>
+        </div>
       </q-card-section>
       <q-card-section class="col right-wrap">
-        <div>카드 기록 공간</div>
+        <!-- <div>카드 기록 공간</div> -->
       </q-card-section>
     </q-card>
   </div>
@@ -18,6 +59,13 @@
 import { onMounted, ref } from 'vue';
 
 const input = ref<string>('');
+
+const result = ref<string>('');
+
+// NOTE: 버튼 클릭
+function onClickValue(value: string) {
+  input.value += value;
+}
 
 // NOTE: life-cycle
 onMounted(() => {
