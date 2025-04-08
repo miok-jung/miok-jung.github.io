e5509efe9a3c0381c1a3f66753ae778c27da7144
miok-jung <miok-jung@users.noreply.github.com>
2025-04-08 07:24:07 +0000

    deploy: 5bac81f8514f2edf0c3414bb3127065d5378bbe5

--- assets/i18n-Cg4vvNAJ.js
+++ assets/i18n-Cg4vvNAJ.js
@@ -1,4 +1,4 @@
-import{e as On,r as kn,s as An,c as Ie,w as it,i as Pn,d as ht,g as cn,h as on,F as un,a as vn,o as Rn,b as Dn,f as Fn,T as Mn,j as wn}from"./index-B1LvDshZ.js";/*!
+import{e as On,r as kn,s as An,c as Ie,w as it,i as Pn,d as ht,g as cn,h as on,F as un,a as vn,o as Rn,b as Dn,f as Fn,T as Mn,j as wn}from"./index-Dma0GmUa.js";/*!
   * shared v11.1.2
   * (c) 2025 kazuya kawaguchi
   * Released under the MIT License.
--- assets/notify-BRw7OuBM.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as o}from"./index-B1LvDshZ.js";const i=t(()=>{o.setDefaults({position:"bottom",timeout:2500,textColor:"white"})});export{i as default};
