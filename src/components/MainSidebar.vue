dba639672a4232fe3f114651912aeae7818a6a28
miok-jung <miok.dev@gmail.com>
2025-04-21 22:56:42 +0900

    feat: sideBar 초기 컴포넌트 첫 수정 업데이트

--- src/components/MainSidebar.vue
+++ src/components/MainSidebar.vue
@@ -1,14 +1,6 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
@@ -20,14 +12,14 @@
 </template>
 
 <script setup lang="ts">
-export interface EssentialLinkProps {
+export interface MainSidebarProps {
   title: string;
   caption?: string;
   link?: string;
   icon?: string;
-};
+}
 
-withDefaults(defineProps<EssentialLinkProps>(), {
+withDefaults(defineProps<MainSidebarProps>(), {
   caption: '',
   link: '#',
   icon: '',
