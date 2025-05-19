62b6a0e4c4d256a3e590290e87f7fafbe4269282
miok-jung <miok.dev@gmail.com>
2025-05-19 22:46:18 +0900

    update: 각 Section별로 높이 업데이트

--- src/pages/side/AppleIndexPage.vue
+++ src/pages/side/AppleIndexPage.vue
@@ -17,7 +17,7 @@
           <a href="#">구입하기</a>
         </div>
       </nav>
-      <section id="scroll-section-0" class="scroll-section">
+      <section ref="section0" id="scroll-section-0" class="scroll-section">
         <h1>AirMug Pro</h1>
         <div class="sticky-elem main-message">
           <p>온전히 빠져들게 하는<br />최고급 세라믹</p>
@@ -32,7 +32,7 @@
           <p>새롭게 입가를<br />찾아온 매혹</p>
         </div>
       </section>
-      <section id="scroll-section-1" class="scroll-section">
+      <section ref="section1" id="scroll-section-1" class="scroll-section">
         <p class="description">
           <strong>보통 스크롤 영역</strong>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, exercitationem nisi?
@@ -55,14 +55,14 @@
           suscipit dolorem! Rem amet cumque dicta sequi ullam.
         </p>
       </section>
-      <section id="scroll-section-2" class="scroll-section">
+      <section ref="section2" id="scroll-section-2" class="scroll-section">
         <div class="sticky-elem main-message">
           <p>
             <small>편안한 촉감</small>
             입과 하나 되다
           </p>
         </div>
-        <div class="stick-elem desc-message">
+        <div class="sticky-elem desc-message">
           <p>
             편안한 목넘김을 완성하는 디테일한 여러 구성 요소를, 우리는 이를 하나하나 새롭게 살피고
             재구성하는 과정을 거쳐 새로운 수준의 머그, AirMug Pro를 만들었습니다. 입에 뭔가 댔다는
@@ -75,7 +75,7 @@
           <div class="pin"></div>
         </div>
       </section>
-      <section id="scroll-section-3" class="scroll-section">
+      <section ref="section3" id="scroll-section-3" class="scroll-section">
         <p class="mid-message">
           <strong>Retina 머그</strong><br />
           아이디어를 광활하게 펼칠<br />
@@ -107,7 +107,82 @@
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuasar } from "quasar";
+import { onMounted, ref } from "vue";
+
+type scrollInformation = {
+  id: number;
+  type: "sticky" | "normal";
+  heightNum: number;
+  scrollHeight: number;
+  objs: {
+    container: HTMLElement | null;
+  };
+};
+const $q = useQuasar();
+
+const section0 = ref<HTMLElement | null>(null);
+const section1 = ref<HTMLElement | null>(null);
+const section2 = ref<HTMLElement | null>(null);
+const section3 = ref<HTMLElement | null>(null);
+const sections = ref<HTMLElement[] | null[]>([null, null, null, null]);
+const screenInfo = ref<scrollInformation[]>([
+  {
+    id: 0,
+    type: "sticky",
+    heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+    scrollHeight: 0,
+    objs: {
+      container: null,
+    },
+  },
+  {
+    id: 1,
+    type: "normal",
+    heightNum: 5,
+    scrollHeight: 0,
+    objs: {
+      container: null,
+    },
+  },
+  {
+    id: 2,
+    type: "sticky",
+    heightNum: 5,
+    scrollHeight: 0,
+    objs: {
+      container: null,
+    },
+  },
+  {
+    id: 3,
+    type: "sticky",
+    heightNum: 5,
+    scrollHeight: 0,
+    objs: {
+      container: null,
+    },
+  },
+]);
+
+// NOTE: LIFE-CYCLE
+onMounted(() => {
+  screenInfo.value.forEach((info, i) => {
+    if (info === null) throw new Error(`scroll-section-${i} 에러발생`);
+    info.scrollHeight = info.heightNum * $q.screen.height;
+
+    if (i === 0) sections.value[i] = section0.value;
+    else if (i === 1) sections.value[i] = section1.value;
+    else if (i === 2) sections.value[i] = section2.value;
+    else sections.value[i] = section3.value;
+
+    if (sections.value[i]) {
+      sections.value[i].style.height = `${info.scrollHeight}px`;
+    }
+  });
+});
+</script>
 <style lang="scss">
-@import 'src/css/side/appleApp.scss';
+@import "src/css/side/appleApp.scss";
 </style>
