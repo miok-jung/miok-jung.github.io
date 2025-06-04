20199246c6bdd68f22cfb654fe75470f53fc29e1
miok-jung <miok.dev@gmail.com>
2025-06-04 22:34:29 +0900

    update: HTML 및 Header CSS

--- src/pages/side/AppleIndexPage.vue
+++ src/pages/side/AppleIndexPage.vue
@@ -1,7 +1,170 @@
 <template>
-  <div>Apple Index Page</div>
+  <div class="container">
+    <nav class="global-nav">
+      <div class="global-nav-links">
+        <a href="#" class="global-nav-item">Rooms</a>
+        <a href="#" class="global-nav-item">Ideas</a>
+        <a href="#" class="global-nav-item">Stores</a>
+        <a href="#" class="global-nav-item">Contact</a>
+      </div>
+    </nav>
+    <nav class="local-nav">
+      <div class="local-nav-links">
+        <a class="product-name" href="#"> AigMug Pro</a>
+        <a href="#">개요</a>
+        <a href="#">제품사양</a>
+        <a href="#">구입하기</a>
+      </div>
+    </nav>
+    <!-- SECTION: SECTION 0 -->
+    <section id="scroll-section-0" class="scroll-section">
+      <h1>AirMug Pro</h1>
+      <div class="sticky-elem main-message">
+        <p>온전히 빠져들게 하는<br />최고급 세라믹</p>
+      </div>
+      <div class="sticky-elem main-message">
+        <p>주변 맛을 느끼게 해주는<br />주변 맛 허용 모드</p>
+      </div>
+      <div class="sticky-elem main-message">
+        <p>온종일 편안한<br />맞춤형 손잡이</p>
+      </div>
+      <div class="sticky-elem main-message">
+        <p>새롭게 입가를<br />찾아온 매혹</p>
+      </div>
+    </section>
+    <!-- !SECTION END -->
+    <!-- SECTION: SECTION 1 -->
+    <section id="scroll-section-1" class="scroll-section">
+      <p>
+        <strong>보통 스크롤 영역</strong>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo sint soluta molestias ratione
+        suscipit, ex quisquam asperiores tempora dolore quia distinctio quis fugit nobis
+        reprehenderit voluptatem est eveniet harum? Aperiam omnis dicta velit, obcaecati totam
+        voluptatibus nihil corporis corrupti iure veritatis quas blanditiis repellat vitae quod.
+        Quos, dignissimos quia? Natus, eos? Eius ipsam earum voluptates incidunt delectus autem
+        consectetur similique. Mollitia, nemo quia! Deleniti laboriosam, amet sunt magni tempore
+        nobis a iusto saepe, distinctio id quidem ex numquam asperiores officiis minus iste beatae.
+        Sunt nam aliquam ut voluptatem ad repellat quos, tenetur assumenda, cum totam enim, maiores
+        iusto at quaerat esse in harum veniam dolores voluptates. Voluptatum excepturi magni
+        inventore numquam qui rem recusandae sequi est. Omnis voluptatibus quasi est reiciendis
+        culpa officiis, excepturi labore suscipit eaque voluptate ipsam nobis, quaerat adipisci
+        perferendis maiores officia ipsum facilis soluta nihil dolorum iure dolor alias eius.
+        Voluptates nostrum quod inventore reiciendis nulla asperiores quisquam id dicta consequatur.
+        Similique omnis non blanditiis quos recusandae magni labore, laudantium numquam facere
+        adipisci quis repellat fugit ad perferendis nisi facilis officiis illum dignissimos fuga
+        cupiditate doloribus eos ipsam dolor architecto! Porro eaque quos deleniti, id, laudantium
+        dolorem enim doloribus labore, minus iste magnam ducimus corrupti qui.
+      </p>
+    </section>
+    <!-- !SECTION END -->
+    <!-- SECTION: SECTION 2 -->
+    <section id="scroll-section-2" class="scroll-section">
+      <div class="sticky-elem main-message">
+        <p>
+          편안한 목넘김을 완성하는 디테일한 여러 구성 요소들, 우리는 이를 하나하나 새롭게 살피고
+          재구성하는 과정을 거쳐 새로운 수준의 머그, AirMug Pro를 만들었습니다. 입에 뭔가 댔다는
+          감각은 어느새 사라지고 오롯이 당신과 음료만 남게 되죠.
+        </p>
+        <div class="pin"></div>
+      </div>
+      <div class="sticky-elem desc-message c">
+        <p>디자인 앤 퀄리티 오브 스웨덴,<br />메이드 인 차이나</p>
+        <div class="pin"></div>
+      </div>
+    </section>
+    <!-- !SECTION END -->
+    <!-- SECTION: SECTION 3 -->
+    <section id="scroll-section-3" class="scroll-section">
+      <p class="mid-message">
+        <strong>Retina 머그</strong><br />
+        아이디어를 광활하게 펼칠<br />
+        아름답고 부드러운 음료 공간.
+      </p>
+      <canvas class="image-blend-canvas" width="1920" height="1080"></canvas>
+      <p class="canvas-caption">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet at fuga quae perspiciatis
+        veniam impedit et, ratione est optio porro. Incidunt aperiam nemo voluptas odit quisquam
+        harum in mollitia. Incidunt minima iusto in corporis, dolores velit. Autem, sit dolorum
+        inventore a rerum distinctio vero illo magni possimus temporibus dolores neque adipisci,
+        repudiandae repellat. Ducimus accusamus similique quas earum laborum. Autem tempora
+        repellendus asperiores illum ex! Velit ea corporis odit? Ea, incidunt delectus. Sapiente
+        rerum neque error deleniti quis, et, quibusdam, est autem voluptate rem voluptas. Ratione
+        soluta similique harum nihil vel. Quas inventore perferendis iusto explicabo animi eos
+        ratione obcaecati.
+      </p>
+    </section>
+    <!-- !SECTION END -->
+    <footer class="footer">2025, Jung, MiOk</footer>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+@import url("../../css/side/appleDefault.scss");
+@import "https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap";
+
+* {
+  overflow-x: hidden;
+  color: rgb(29, 29, 31);
+  font-family: "Noto Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-variation-settings: "wdth" 100;
+  letter-spacing: -0.05em;
+}
+p {
+  line-height: 1.6;
+}
+</style>
+<style scoped lang="scss">
+.container {
+  nav {
+    .global-nav-links,
+    .local-nav-links {
+      display: flex;
+      align-items: center;
+      margin: 0 auto;
+      max-width: 1280px;
+      width: 100%;
+      height: 100%;
+      a {
+        color: rgb(29, 29, 31);
+        text-decoration: none;
+      }
+    }
+    &.global-nav {
+      height: 44px;
+    }
+    &.local-nav {
+      height: 52px;
+      border-bottom: 1px solid #ddd;
+    }
+    div {
+      &.global-nav-links {
+        justify-content: space-between;
+        .global-nav-item {
+        }
+      }
+      &.local-nav-links {
+        a {
+          font-size: 0.8rem;
+          &.product-name {
+            margin-right: auto;
+            font-size: 1.2rem;
+            font-weight: bold;
+          }
+          &:not(.product-name) {
+            margin-left: 2em;
+          }
+        }
+      }
+    }
+  }
+  section {
+    &.scroll-section {
+    }
+  }
+}
+</style>
