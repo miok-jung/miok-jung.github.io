701fe42c4089c1a0ac91e252fd90a596002109c0
miok-jung <miok-jung@gmail.com>
2025-06-23 17:20:44 +0900

    feat: 다이얼로그 테스트 버튼 생성

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,21 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <router-view></router-view>
+  <q-page>
+    <div style="border: 1px solid red">
+      <q-btn label="Click Dialog Test" @click="openDialog = true"></q-btn>
+    </div>
+    <q-dialog v-model="openDialog">
+      <q-card>
+        <q-card-section>다이얼로그 열기 완료</q-card-section>
+        <q-card-actions>
+          <q-btn label="닫기" @click="openDialog = false"></q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const openDialog = ref<boolean>(false);
+</script>
