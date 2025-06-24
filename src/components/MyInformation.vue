396453c0c1b9e866b46e508f4b2bf2ada9fb7310
miok-jung <miok-jung@gmail.com>
2025-06-24 16:29:58 +0900

    feat: ScrollArea 공통 컴포넌트 생성

---
+++ src/components/MyInformation.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="my_profile-wrap">
+    <section class="my_profile">
+      <dl>
+        <dt>Name</dt>
+        <dd>정미옥 (Jung, Mi Ok)</dd>
+        <dt>Email</dt>
+        <dd>miok.dev@gmail.com</dd>
+        <dt>GitHub</dt>
+        <dd>https://github.com/miok-jung</dd>
+      </dl>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.my_profile-wrap {
+  padding: 24px 36px;
+  section.my_profile {
+    border: 1px solid red;
+    dl {
+      border: 1px solid pink;
+    }
+    dt {
+      display: inline-block;
+      width: 20%;
+      border: 1px solid lime;
+    }
+    dd {
+      display: inline-block;
+      width: 80%;
+      border: 1px solid skyblue;
+    }
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,12 @@
 <template>
   <q-page>
-    <div style="border: 1px solid red">
-      <q-btn label="Click Dialog Test" @click="openDialog = true"></q-btn>
-    </div>
-    <q-dialog v-model="openDialog">
-      <q-card>
-        <q-card-section>다이얼로그 열기 완료</q-card-section>
-        <q-card-actions>
-          <q-btn label="닫기" @click="openDialog = false"></q-btn>
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+    <BaseScrollArea :horizontal="true" :vertical="true" style="height: 100px">
+      <MyInformation />
+    </BaseScrollArea>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
-const openDialog = ref<boolean>(false);
+import BaseScrollArea from "src/components/common/BaseScrollArea.vue";
+import MyInformation from "src/components/MyInformation.vue";
 </script>
