f88fbf2880195562762ac8d64734d5293341eb20
miok-jung <miok-jung@gmail.com>
2025-06-30 17:53:12 +0900

    fix: MyInformation.vue를 Profile.vue로 변경

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page>
     <BaseScrollArea :horizontal="true" :vertical="true" style="height: calc(100vh - 50px)">
-      <MyInformation />
+      <Profile />
     </BaseScrollArea>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import BaseScrollArea from 'src/components/common/BaseScrollArea.vue';
-import MyInformation from 'src/components/MyInformation.vue';
+import Profile from 'src/components/Profile.vue';
 </script>
