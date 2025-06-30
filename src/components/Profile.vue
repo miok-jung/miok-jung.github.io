bd46355945027e3ada94a1a3872295c612103f41
miok-jung <miok.dev@gmail.com>
2025-07-01 00:16:53 +0900

    feat: 프로파일 링크 추가

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,18 +3,37 @@
     <section class="my_profile">
       <q-img class="avatar shadow-2" :src="avatar" width="150px" height="150px"></q-img>
       <dl>
-        <div class="description-list">
-          <dt>Name</dt>
-          <dd>정미옥 (Jung, Mi Ok)</dd>
-        </div>
-        <div class="description-list">
-          <dt>Email</dt>
-          <dd>miok.dev@gmail.com</dd>
-        </div>
-        <div class="description-list">
-          <dt>GitHub</dt>
-          <dd>https://github.com/miok-jung</dd>
-        </div>
+        <dt>Name</dt>
+        <dd>정미옥 (Jung, Mi Ok)</dd>
+        <dt>Email</dt>
+        <dd>miok.dev@gmail.com</dd>
+        <dt>GitHub</dt>
+        <dd>https://github.com/miok-jung</dd>
+        <dt>Link</dt>
+        <dd class="q-gutter-x-sm">
+          <q-btn
+            icon="mdi-alpha-b-circle-outline"
+            target="_blank"
+            href="https://jnarin-development-story.tistory.com"
+            type="a"
+            flat
+            dense
+            rounded
+          >
+            <q-tooltip>블로그 바로가기</q-tooltip>
+          </q-btn>
+          <q-btn
+            icon="mdi-github"
+            target="_blank"
+            href="https://github.com/miok-jung"
+            type="a"
+            flat
+            dense
+            rounded
+          >
+            <q-tooltip>GitHub 바로가기</q-tooltip>
+          </q-btn>
+        </dd>
       </dl>
     </section>
   </div>
@@ -39,18 +58,15 @@ import avatar from 'assets/images/avatar.png';
       margin-left: 1rem;
       width: calc(100% - 166px);
     }
-    .description-list {
-      &:last-child {
-        margin-bottom: 0;
-      }
-    }
-
     dt {
       display: inline-block;
-      padding-right: 0.5rem;
+      padding: 0.5rem 0.5rem 0 0;
       width: 20%;
       text-align: right;
       border-right: 1px solid #aaa;
+      &:last-child {
+        padding: 0 0.5rem 0 0;
+      }
     }
     dd {
       display: inline-block;
