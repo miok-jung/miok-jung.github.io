a173d0b9878921e0482f71fce7f1da1ca5b57127
miok-jung <miok.dev@gmail.com>
2025-07-01 23:47:40 +0900

    feat: Dark/Light MODE 토글 추가

--- src/components/main/Header.vue
+++ src/components/main/Header.vue
@@ -3,13 +3,27 @@
     <q-toolbar>
       <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
       <q-toolbar-title> Miok, Jung's Portfolio </q-toolbar-title>
+      <q-toggle
+        v-model="isDarkMode"
+        checked-icon="mdi-brightness-4"
+        unchecked-icon="mdi-brightness-5"
+        :color="isDarkMode ? 'indigo-10' : 'yellow-10'"
+        keep-color
+        @update:model-value="changeDarkMode"
+      >
+        <q-tooltip>Change {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}</q-tooltip>
+      </q-toggle>
     </q-toolbar>
   </q-header>
 </template>
 <script setup lang="ts">
+import { useQuasar } from 'quasar';
+import { ref } from 'vue';
+
 interface Props {
   sidebar: boolean;
 }
+const { dark } = useQuasar();
 
 const props = withDefaults(defineProps<Props>(), {
   sidebar: false,
@@ -17,9 +31,13 @@ const props = withDefaults(defineProps<Props>(), {
 const emit = defineEmits<{
   (e: 'changeSidebar', value: boolean): void;
 }>();
-
 function toggleLeftDrawer() {
   emit('changeSidebar', !props.sidebar);
 }
+
+const isDarkMode = ref<boolean>(false);
+const changeDarkMode = () => {
+  dark.set(isDarkMode.value);
+};
 </script>
 <style scoped lang="scss"></style>
