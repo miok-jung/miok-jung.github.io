c17e9895c0008d9f3ffc188b5e3b8850b35500f4
miok-jung <miok-jung@gmail.com>
2025-07-10 14:16:29 +0900

    feat: QTable TEST

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,10 +1,14 @@
 <template>
   <q-page :style-fn="TestPageStyles">
     <h5>Test PAGE</h5>
+    <p>css 라이브러리에서 쓰이는 Quasar table</p>
+    <QuasarTable />
   </q-page>
 </template>
 
 <script setup lang="ts">
+import QuasarTable from 'src/components/test/QuasarTable.vue';
+
 // 전체 페이지 스타일
 const TestPageStyles = () => {
   return { padding: '1rem' };
