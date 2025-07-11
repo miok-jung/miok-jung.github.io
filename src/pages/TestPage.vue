ef9f85e600ffd69bde7fed4cc807d3cc213a2aa0
miok-jung <miok-jung@gmail.com>
2025-07-11 14:14:05 +0900

    feat: table HTML TEST

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -3,11 +3,14 @@
     <h5>Test PAGE</h5>
     <p>css 라이브러리에서 쓰이는 Quasar table</p>
     <QuasarTable />
+    <p>Quasar Table을 대체하기 위해 수작업 ING</p>
+    <BaseTable :thead="['Person', 'Most interest in', 'Age']" :tbody="['']" />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import QuasarTable from 'src/components/test/QuasarTable.vue';
+import BaseTable from 'src/components/test/BaseTable.vue';
 
 // 전체 페이지 스타일
 const TestPageStyles = () => {
