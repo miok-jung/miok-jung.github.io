378ab61ded6192871bd6693766e330c4210d9c17
miok-jung <miok-jung@users.noreply.github.com>
2025-07-15 12:51:22 +0000

    deploy: 36f72684895204791fa0ef2e1a32513c764e6049

--- assets/notify-lYxC8yRQ.js
+++
@@ -1,1 +0,0 @@
-import{d as t,N as o}from"./index-CTG9l_2f.js";const i=t(()=>{o.setDefaults({position:"bottom",timeout:2500,textColor:"white"})});export{i as default};
--- assets/use-quasar-DzAtJ9Hm.js
+++
@@ -1,1 +0,0 @@
-import{i as a,ba as r}from"./index-CTG9l_2f.js";function u(){return a(r)}export{u};
