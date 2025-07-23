f90ec5aae45d9ed7dab27bc0bafd4381a8d6d37b
miok-jung <miok-jung@users.noreply.github.com>
2025-07-23 14:06:50 +0000

    deploy: 58e3618af6914d121358a70f2f7a9e6adc3be165

---
+++ assets/notify-B_cifFzd.js
@@ -0,0 +1,1 @@
+import{d as t,N as o}from"./index-Kx0k7DSF.js";const i=t(()=>{o.setDefaults({position:"bottom",timeout:2500,textColor:"white"})});export{i as default};
---
+++ assets/use-quasar-DEtPifDB.js
@@ -0,0 +1,1 @@
+import{i as a,bb as r}from"./index-Kx0k7DSF.js";function u(){return a(r)}export{u};
