3727b55d93820117cd2f9a5a37c489c1a775c716
miok-jung <miok.dev@gmail.com>
2025-07-27 15:48:36 +0900

    update: 스크롤 정보 업데이트

--- src/pages/side/apple/IndexPage.vue
+++ src/pages/side/apple/IndexPage.vue
@@ -25,16 +25,16 @@
         :style="{ height: sceneInfo[0]?.scrollHeight + 'px' }"
       >
         <h1>AirMug Pro</h1>
-        <div class="sticky-elem main-message">
+        <div ref="mainMessageA" class="sticky-elem main-message">
           <p>온전히 빠져들게 하는<br />최고급 세라믹</p>
         </div>
-        <div class="sticky-elem main-message">
+        <div ref="mainMessageB" class="sticky-elem main-message">
           <p>주변 맛을 느끼게 해주는<br />주변 맛 허용 모드</p>
         </div>
-        <div class="sticky-elem main-message">
+        <div ref="mainMessageC" class="sticky-elem main-message">
           <p>온종일 편안한<br />맞춤형 손잡이</p>
         </div>
-        <div class="sticky-elem main-message">
+        <div ref="mainMessageD" class="sticky-elem main-message">
           <p>새롭게 입가를<br />찾아온 매혹</p>
         </div>
       </section>
@@ -143,42 +143,66 @@ const pageStyles = () => {
   };
 };
 
-// NOTE: Scene 정보
+// NOTE: Scene 정보 업데이트 및 세팅
 type Scene = {
   type: 'sticky' | 'normal';
   heightNum: number; // 스크롤 높이의 배수값
   scrollHeight: number; // 스크롤 높이
-  objs: HTMLElement | undefined;
+  objs: {
+    container: HTMLElement | undefined;
+    mainMessageA?: HTMLElement | undefined;
+    mainMessageB?: HTMLElement | undefined;
+    mainMessageC?: HTMLElement | undefined;
+    mainMessageD?: HTMLElement | undefined;
+  };
+  values?: {
+    messageA_opacity: [number, number];
+  };
 };
 const scene1 = ref<HTMLElement | undefined>(undefined);
+const mainMessageA = ref<HTMLElement | undefined>(undefined);
+const mainMessageB = ref<HTMLElement | undefined>(undefined);
+const mainMessageC = ref<HTMLElement | undefined>(undefined);
+const mainMessageD = ref<HTMLElement | undefined>(undefined);
 const scene2 = ref<HTMLElement | undefined>(undefined);
 const scene3 = ref<HTMLElement | undefined>(undefined);
 const scene4 = ref<HTMLElement | undefined>(undefined);
-
 const sceneInfo = ref<Scene[]>([
   {
     type: 'sticky',
     heightNum: 5,
     scrollHeight: 0,
-    objs: undefined,
+    objs: {
+      container: undefined,
+      mainMessageA: undefined,
+      mainMessageB: undefined,
+      mainMessageC: undefined,
+      mainMessageD: undefined,
+    },
   },
   {
     type: 'normal',
     heightNum: 5,
     scrollHeight: 0,
-    objs: undefined,
+    objs: {
+      container: undefined,
+    },
   },
   {
     type: 'sticky',
     heightNum: 5,
     scrollHeight: 0,
-    objs: undefined,
+    objs: {
+      container: undefined,
+    },
   },
   {
     type: 'sticky',
     heightNum: 5,
     scrollHeight: 0,
-    objs: undefined,
+    objs: {
+      container: undefined,
+    },
   },
 ]);
 function setLayout() {
@@ -188,7 +212,7 @@ function setLayout() {
     const scene = sceneInfo.value[i];
     if (scene) {
       scene.scrollHeight = scene.heightNum * screen.height;
-      scene.objs = sceneObjs[i];
+      scene.objs.container = sceneObjs[i];
     }
   }
   let totalScrollHeight = 0;
@@ -204,6 +228,8 @@ function setLayout() {
   }
   document.body.setAttribute('id', `show-scene-${currentScene.value}`);
 }
+
+// NOTE: 스크롤 정보 업데이트
 const yOffset = ref<number>(0); // window.pageYOffset 대신 쓸 변수
 const prevScrollHeight = ref<number>(0); // 현재 스크롤 위치(yOffset) 보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
 const currentScene = ref<number>(0);
@@ -211,6 +237,37 @@ const onScroll = (pos: number) => {
   yOffset.value = pos;
   scrollLoop();
 };
+
+// function calcValue(values: number[], currentYOffset: number) {}
+function playAnimation() {
+  const scene = sceneInfo.value[currentScene.value];
+  if (scene === undefined) return;
+
+  // const objs = scene.objs;
+  const values = scene.values;
+  if (values === undefined) return;
+  switch (currentScene.value) {
+    case 0: {
+      const messageA_opacity_0 = values.messageA_opacity[0];
+      const messageA_opacity_1 = values.messageA_opacity[1];
+
+      console.log('play 0', messageA_opacity_0, messageA_opacity_1);
+      break;
+    }
+    case 1: {
+      console.log('play 1');
+      break;
+    }
+    case 2: {
+      console.log('play 2');
+      break;
+    }
+    case 3: {
+      console.log('play 3');
+      break;
+    }
+  }
+}
 const scrollLoop = () => {
   prevScrollHeight.value = 0;
 
@@ -232,6 +289,8 @@ const scrollLoop = () => {
     currentScene.value--;
     document.body.setAttribute('id', `show-scene-${currentScene.value}`);
   }
+  // mainMessage 애니메이션 실행
+  playAnimation();
 };
 
 // NOTE: watch
