a27ac4d60f207fbbb97dfae20280e27c2c214623
miok-jung <miok-jung@gmail.com>
2025-11-27 14:38:31 +0900

    update: Router 사용 및 layout/page 구분하도록 테스트

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,20 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import HomeLayout from "../layouts/HomeLayout.vue";
+import HomePages from "../pages/HomePage.vue";
 
-const routes = [{ path: "/", component: HomeLayout }];
+const routes = [
+  {
+    path: "/",
+    component: HomeLayout,
+    children: [
+      {
+        path: "",
+        component: HomePages,
+      },
+    ],
+  },
+];
 
 export const router = createRouter({
   history: createMemoryHistory(),
