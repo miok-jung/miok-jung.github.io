0544208527adc3e0a2227a5404df3da78a327373
miok-jung <miok-jung@gmail.com>
2025-12-04 11:45:11 +0900

    feat: 프로그램 사용 모듈

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      components: path.resolve(__dirname, 'src/components'),
+      pages: path.resolve(__dirname, 'src/pages'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
 })
