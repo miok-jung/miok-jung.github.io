0f7c64eda4005474367770903b10a35e6abeb1d6
miok-jung <miok-jung@gmail.com>
2026-01-02 10:57:39 +0900

    theme 클래스명 업데이트

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const el = document.documentElement
+  el.classList.remove('theme-dark')
+  el.classList.add('theme-light')
+})
+</script>
 <template>
   <router-view />
 </template>
