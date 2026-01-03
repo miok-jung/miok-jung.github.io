d60dd525954ad6fe2189d81d77d27d89d3c15045
miok-jung <miok.dev@gmail.com>
2026-01-03 23:37:43 +0900

    테스트 라우터 추가

---
+++ src/pages/TestPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="test-wrap">
+    <h2>Test Page</h2>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.test-wrap {
+  padding: 2rem;
+}
+</style>
