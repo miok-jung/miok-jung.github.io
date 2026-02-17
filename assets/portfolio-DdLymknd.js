d2ead27612004afcebe0d7d1dd1f031be0e32bcc
miok-jung <miok-jung@users.noreply.github.com>
2026-02-17 02:00:51 +0000

    deploy: 7e184d931f3324509f701308825a3d371bdd2738

---
+++ assets/portfolio-DdLymknd.js
@@ -0,0 +1,1 @@
+const o={portfolio:{home:{title:"홈",description:"클릭하면 해당 페이지로 이동합니다"}}};export{o as default};
--- assets/portfolio-IAr2-61M.js
+++
@@ -1,1 +0,0 @@
-const o={portfolio:{title:"홈",description:"클릭하면 해당 페이지로 이동합니다"}};export{o as default};
