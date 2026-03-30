fb1bf97c88153bfe8dc63de921365da94a41e984
miok-jung <miok-jung@users.noreply.github.com>
2026-03-30 12:44:24 +0000

    deploy: 68c55b1bb37a6d58b2438eb0be3accc64acdc03e

---
+++ assets/portfolio-BeyFFZ7d.js
@@ -0,0 +1,2 @@
+const o={portfolio:{home:{title:"보이는 것 이상의 것을 만듭니다",description:`사용자 경험을 해치지 않는 퍼블리싱,
+유지보수 가능한 UI 구조, 그리고 "왜 이렇게 만들었는지..." 설명 가능한 코드를 좋아합니다.`,pages:{test:"테스트 페이지",hanacard:"하나카드 페이지",cj_one:"CJ ONE 페이지"}},about:{labels:{company:"회사",career:"경력",period:"근무기간",duration:"근속기간",role:"담당업무",projects:"프로젝트"},careers:[{company_name:"(주)아이팩토리",period:{from:"2022-07-17T15:00:00Z",to:""},projects:[{id:"draft",name:"Draft"}]},{company_name:"(주)뉴이스트아트(믐)",period:{from:"",to:""},projects:[{id:"draft",name:"Draft"}]}]}}};export{o as default};
