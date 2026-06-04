15bb5935d9ac0802059cb84db5821d4d7f459bce
miok-jung <miok.dev@gmail.com>
2026-06-04 20:06:20 +0900

    fix: 테스트 페이지 커스텀 버튼 컴포넌트 보더 제거

--- src/components/test/Buttons.vue
+++ src/components/test/Buttons.vue
@@ -7,17 +7,19 @@ import BaseButton from '../common/BaseButton.vue'
       <h3>A Tag</h3>
       <div class="component__content">
         <a href="/test">기본 링크</a>
-        <BaseButton href="/test">커스텀 링크</BaseButton>
-        <BaseButton href="/test" color="primary">커스텀 링크</BaseButton>
-        <BaseButton href="/test" type="outline" color="primary">커스텀 링크</BaseButton>
-        <BaseButton href="/test" type="outline" color="primary">커스텀 링크</BaseButton>
+        <BaseButton href="/test">커스텀 default 링크</BaseButton>
+        <BaseButton href="/test" color="primary">커스텀 flat 링크</BaseButton>
+        <BaseButton href="/test" type="outline" color="primary">커스텀 outline 링크</BaseButton>
       </div>
     </div>
-    <div>
-      <button>기본 버튼</button>
-      <BaseButton>Base Button</BaseButton>
-      <BaseButton color="primary">Primary FLat Button</BaseButton>
-      <BaseButton type="outline" color="primary">Primary Outline Button</BaseButton>
+    <div class="component__content-wrap">
+      <h3>Button Tag</h3>
+      <div class="component__content">
+        <button>기본 버튼</button>
+        <BaseButton>커스텀 default 버튼</BaseButton>
+        <BaseButton color="primary">커스텀 flat 버튼</BaseButton>
+        <BaseButton type="outline" color="primary">커스텀 outline 버튼</BaseButton>
+      </div>
     </div>
   </div>
 </template>
@@ -29,16 +31,18 @@ import BaseButton from '../common/BaseButton.vue'
     display: flex;
     flex-direction: column;
     gap: 8px;
-    border: 1px solid blue;
+    /* border: 1px solid blue; */
   }
   &__content-wrap {
-    border: 1px solid lime;
+    /* border: 1px solid lime; */
   }
   &__content {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    display: flex;
+    /* display: grid;
+    grid-template-columns: repeat(5, auto); */
+    gap: 24px;
     margin: 2rem 0;
-    border: 1px solid red;
+    /* border: 1px solid red; */
   }
 }
 </style>
